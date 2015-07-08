61fdb070efc79615980f33d5f234ce3b838292e4
JXC <johnxcoulter@gmail.com>
2015-07-07 23:44:37 -0400

    more configs

--- iterm/install.sh
+++ iterm/install.sh
@@ -6,6 +6,7 @@
 
 open -a '/Applications/iTerm.app' "$HOME/.dotfiles/iterm/Solarized Light.itermcolors"
 open -a '/Applications/iTerm.app' "$HOME/.dotfiles/iterm/Solarized Dark.itermcolors"
+open -a '/Applications/iTerm.app' "$HOME/.dotfiles/iterm/FlatUI-Palette.itermcolors"
 
 # Make colors work
 /usr/libexec/PlistBuddy -c "Set :New\ Bookmarks:0:Use\ Bright\ Bold 1" ~/Library/Preferences/com.googlecode.iterm2.plist
