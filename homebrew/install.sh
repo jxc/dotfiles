8845ce3497daae6c8593b7b1eb230885b5635a89
JXC <johnxcoulter@gmail.com>
2015-07-08 00:05:42 -0400

    COWSAY

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # Install homebrew packages
-brew install ack autojump caskroom/cask/brew-cask coreutils findutils git openssl xctool zsh
+brew install ack autojump caskroom/cask/brew-cask coreutils findutils git openssl xctool zsh cowsay
 
 # Make sure we have chrome
 brew cask install google-chrome
