bb6e23cc108338e1d7b0b8cabfcba7a48fd06683
Tyler Wolf <tdwolf@gmail.com>
2016-01-30 18:27:46 -0800

    Added brew casks

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,14 +5,46 @@
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Check for Homebrew
-if test ! $(which brew)
+if test $(which brew)
 then
+  brew update
+  brew upgrade
+else
   echo "  Installing Homebrew for you."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Install homebrew packages
-brew install ack autojump brew-cask coreutils findutils git openssl xctool zsh
+# Tap taps
+tapped_taps=$(brew tap -1)
+for t in `cat ${DIR}/taps.txt`; do
+    if ! [ ! -z $(echo "${tapped_taps}" | grep "^${t}\$") ]
+    then
+        brew tap ${t}
+    fi
+done
+
+# Install Homebrew formulae
+installed_formulae=$(brew list -1)
+for f in `cat ${DIR}/formulae.txt`; do
+    if ! [ ! -z $(echo "${installed_formulae}" | grep "^${f}\$") ]
+    then
+        brew install ${f}
+    fi
+done
+
+# Install casks
+installed_casks=$(brew cask list -1)
+for c in `cat ${DIR}/casks.txt`; do
+    if ! [ ! -z $(echo "${installed_casks}" | grep "^${c}\$") ]
+    then
+        brew cask install ${c}
+    fi
+done
+
+# Cleanup
+brew cask cleanup
 
 exit 0
