3ad1e94d18507f570a2898a2153ed3e2fb2b929f
Tyler Wolf <tdwolf@gmail.com>
2016-01-30 20:36:00 -0800

    Switched to using homebrew-bundle

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -11,40 +11,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if test $(which brew)
 then
   brew update
-  brew upgrade
+  brew bundle --global
+  brew cask cleanup
 else
   echo "  Installing Homebrew for you."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-
-# Tap taps
-tapped_taps=$(brew tap -1)
-for t in `cat ${DIR}/Tapfile | grep -v "#"`; do
-    if ! [ ! -z $(echo "${tapped_taps}" | grep "^${t}\$") ]
-    then
-        brew tap ${t}
-    fi
-done
-
-# Install Homebrew formulae
-installed_formulae=$(brew list -1)
-for f in `cat ${DIR}/Brewfile | grep -v "#"`; do
-    if ! [ ! -z $(echo "${installed_formulae}" | grep "^${f}\$") ]
-    then
-        brew install ${f}
-    fi
-done
-
-# Install casks
-installed_casks=$(brew cask list -1)
-for c in `cat ${DIR}/Caskfile | grep -v "#"`; do
-    if ! [ ! -z $(echo "${installed_casks}" | grep "^${c}\$") ]
-    then
-        brew cask install ${c}
-    fi
-done
-
-# Cleanup
-brew cask cleanup
 
 exit 0
