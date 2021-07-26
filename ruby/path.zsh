cfc3dff7d51a496c7ed05120eeda90bf807eb4e8
Maximilian <johnxcoulter@gmail.com>
2021-07-26 17:34:20 -0600

    Move from chruby to rbenv

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,2 +1,4 @@
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
+# source /usr/local/opt/chruby/share/chruby/chruby.sh
+# source /usr/local/opt/chruby/share/chruby/auto.sh
+
+export PATH="$HOME/.rbenv/bin:$PATH"
