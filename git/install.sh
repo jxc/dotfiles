afd1158ee41b1ce5c25740809a2d03fe3fef17a3
Tyler Wolf <tdwolf@gmail.com>
2023-03-30 11:49:00 -0600

    Disabling git credential helper settings to test out 1Password integration

--- git/install.sh
+++ git/install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Don't ask ssh password all the time
-if [ "$(uname -s)" = "Darwin" ]; then
-	git config --global credential.helper osxkeychain
-else
-	git config --global credential.helper cache
-fi
+# if [ "$(uname -s)" = "Darwin" ]; then
+# 	git config --global credential.helper osxkeychain
+# else
+# 	git config --global credential.helper cache
+# fi
 
 # better diffs
 if which diff-so-fancy >/dev/null 2>&1; then
