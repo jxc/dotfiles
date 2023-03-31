d041c4dc4a8c47961e87be3f858c2d1390756c85
tylerwolf <twolf@hashicorp.com>
2023-03-31 12:20:29 -0600

    Add asdf

---
+++ asdf/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install asdf || brew upgrade asdf
---
+++ asdf/setup.zsh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+. $(brew --prefix asdf)/libexec/asdf.sh
