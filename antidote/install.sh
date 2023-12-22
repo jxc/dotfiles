a3a7ee4ae654707818ca88500cbc982eef753b0f
Maximilian Coulter <johnxcoulter@gmail.com>
2023-12-22 10:55:26 -0700

    Think antidote is happy now

--- antidote/install.sh
+++ antidote/install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install antidote || brew upgrade antidote
-antidote bundle <"$DOTFILES/antidote/bundles.txt" >~/.zsh_plugins.sh
-antidote update
